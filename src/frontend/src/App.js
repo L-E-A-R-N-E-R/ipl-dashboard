55cb11e550e2deb3f3609079e5c6a5c1d89c4507
L-E-A-R-N-E-R <porwalmohit1999@gmail.com>
2023-08-31 22:28:48 -0700

    Adding React Routes and Links for Teams

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {TeamPage} from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
+      <Router>
+        <Routes>
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
